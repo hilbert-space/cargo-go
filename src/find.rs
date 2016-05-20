de0b325e33a865baeaa20f201b19fd0b4a83ad59
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-05-20 11:06:27 +0200

    Implement a basic argument handling

--- src/find.rs
+++ src/find.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 pub fn find(_: &str, _: &str) -> Option<String> {
     None
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
-use std::process;
+use std::{env, process};
 
 mod find;
 mod load;
 mod open;
 
+enum Destination {
+    Crates,
+    Unknown,
+}
+
 fn main() {
-    let path = "https://crates.io";
-    if let Err(_) = open::open(path) {
-        println!("Failed to open {:?}.", path);
-        process::exit(1);
+    let path = match parse() {
+        None | Some((_, Destination::Unknown)) => error("do not know where to go"),
+        Some((name, Destination::Crates)) => format!("https://crates.io/crates/{}", name),
+    };
+    if let Err(_) = open::open(&path) {
+        error(&format!("failed to go to {:?}", path));
     }
+}
+
+fn parse() -> Option<(String, Destination)> {
+    let mut args = env::args().collect::<Vec<_>>();
+    let (name, destination) = match args.len() {
+        0 | 1 => return None,
+        2 => (args.remove(1), String::new()),
+        _ => (args.remove(1), args.remove(1).to_lowercase()),
+    };
+    let destination = match &*destination {
+        "" | "crates" | "crates.io" => Destination::Crates,
+        _ => Destination::Unknown,
+    };
+    Some((name, destination))
+}
+
+fn error(message: &str) -> ! {
+    println!("Error: {}.", message);
+    process::exit(1);
 }
